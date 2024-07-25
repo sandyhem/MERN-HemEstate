22841a3ff73604f365a70da237da4976e75143b7
sandyhem <santhoshkowsalya2004@gmail.com>
2024-07-25 22:28:59 +0530

    created the signin api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).
@@ -10,10 +11,21 @@ then(()=>{
 }).catch((err)=>console.log(err));
 
 const app=express();
+app.use(express.json());
 
 
+app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const message=err.message||"Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
 app.listen(3000,()=>{
     console.log("Server is running at port 3000")
 })
