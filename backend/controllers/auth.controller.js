5ec6a87107b36946399346c9963add1e48fe6b91
sandyhem <santhoshkowsalya2004@gmail.com>
2024-07-26 10:44:46 +0530

    created the signin api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
-
+import jwt from 'jsonwebtoken'
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const signup=async(req,res,next)=>{
 const {username,email,password}=req.body;
@@ -17,3 +18,35 @@ res.status(201).json({
     // handwritten error
 }
 }
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"User not found"))
+        }
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,"Wrong Credentials!"));
+        }
+        const token=jwt.sign({
+            id:validUser._id},process.env.SECRET,)
+
+        //remove password from response.
+        const {password:pass, ...rest}=validUser._doc;
+        
+       res.cookie('access_token',token,
+        {
+            httpOnly:true
+        }
+       ).status(200).json({
+       rest
+       })
+  
+
+    } catch (error) {
+        next(error);
+    }
+}
+
