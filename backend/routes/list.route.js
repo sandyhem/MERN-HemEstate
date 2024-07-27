2f24fddffcf511b5fb35f987f6357ca42173ac5e
sandyhem <santhoshkowsalya2004@gmail.com>
2024-07-27 22:03:57 +0530

    completed delete userlisting fuctionality

--- backend/routes/list.route.js
+++ backend/routes/list.route.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import { createListing } from '../controllers/list.controller.js';
+import { createListing, deleteListing } from '../controllers/list.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 const router=express.Router();
 
 router.post('/create',verifyUser,createListing)
-
+router.delete('/delete/:id',verifyUser,deleteListing)
 export default router;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -126,6 +126,23 @@ const handleShowListings=async()=>{
     setShowListingError(error.message) 
   }
 }
+
+const handleListingDelete=async(listingId)=>{
+  try {
+    const res=await fetch(`/api/listing/delete/${listingId}`,{
+      method:'DELETE',
+    })
+    const data=await res.json();
+    if(data.success === false){
+      console.log(data.message);
+      return;
+    }
+   setUserListings((prev)=>
+    prev.filter((listing)=>listing._id !== listingId))
+  } catch (error) {
+    console.log(error);
+  }
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center">Profile</h1>
@@ -207,7 +224,7 @@ const handleShowListings=async()=>{
                    <p>{listing.name}</p>
                 </Link>
                    <div className="flex flex-col items-center">
-                     <button className="text-red-700 uppercase">Delete</button>
+                     <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
                      <button className="text-green-700 uppercase">Edit</button>
                    </div>
            </div>)
