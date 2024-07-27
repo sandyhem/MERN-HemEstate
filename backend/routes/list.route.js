b7f34ab8ca2eb01d532546892c5eead929eb769a
sandyhem <santhoshkowsalya2004@gmail.com>
2024-07-27 22:16:24 +0530

    completed updatelisting api

--- backend/routes/list.route.js
+++ backend/routes/list.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { createListing, deleteListing } from '../controllers/list.controller.js';
+import { createListing, deleteListing, updateListing } from '../controllers/list.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 const router=express.Router();
 
 router.post('/create',verifyUser,createListing)
 router.delete('/delete/:id',verifyUser,deleteListing)
+router.post('/update/:id',verifyUser,updateListing)
+
 export default router;
