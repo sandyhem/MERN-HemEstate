5facc457bd060cdadf6f3714cc6b90776e1ec4cb
sandyhem <santhoshkowsalya2004@gmail.com>
2024-07-29 11:35:37 +0530

    completed header search form functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,38 +1,73 @@
-import React from 'react'
-import {FaSearch}from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 export default function Header() {
-  const navigate=useNavigate();
-  const {currentUser}=useSelector((state)=>state.user);
-  return (
- <header className='bg-slate-200 shadow-md'>
-    <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className='text-slate-500'>Hem</span>
-        <span className='text-slate-700'>Estate</span>
-    </h1>
-    <form  className='bg-slate-100 p-3 rounded-lg flex items-center'>
-        <input type="text" placeholder='Search...' className=' border-none bg-transparent focus:outline-none w-24 sm:w-64'/>
-        <FaSearch className='text-slate-600'></FaSearch>
-    </form>
-    <ul className='flex gap-4'>
-        <Link to="">
-        <li className='hidden sm:inline hover:underline cursor-pointer'>Home</li>
-        </Link>
-        <Link to='/about'>
-        <li className='hidden sm:inline hover:underline cursor-pointer'>About</li>
-        </Link>
-        <Link to='/profile'>
-{
-    currentUser?(<img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'></img>):
-    (<li className='sm:inline hover:underline cursor-pointer'>Sign In</li>)
-}
-        
-        </Link>
-    </ul>
-    </div>
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const Navigate=useNavigate();
 
- </header>
-  )
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    const urlParams= new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery=urlParams.toString();
+    Navigate(`/search?${searchQuery}`)
+  }
+
+  //to make url params to access to the search bar
+useEffect(()=>{
+const urlParams=new URLSearchParams(window.location.search);
+const searchTermFromUrl=urlParams.get('searchTerm');
+if(searchTermFromUrl){
+    setSearchTerm(searchTermFromUrl);
+}
+},[location.search])
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">Hem</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className=" border-none bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
+          />
+          <button>
+          <FaSearch className="text-slate-600"></FaSearch>
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="">
+            <li className="hidden sm:inline hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              ></img>
+            ) : (
+              <li className="sm:inline hover:underline cursor-pointer">
+                Sign In
+              </li>
+            )}
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
